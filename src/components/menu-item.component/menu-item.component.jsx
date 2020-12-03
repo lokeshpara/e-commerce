c7f856eb0e0a7c3576bdb34421f4974fbbacc536
lokeshpara <lokeshpara17@gmail.com>
2020-12-03 20:59:31 +0530

    added style to menu

--- src/components/menu-item.component/menu-item.component.jsx
+++ src/components/menu-item.component/menu-item.component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './menu-item.component.style.scss'
 
 const MenuItem = ({title,image,large}) => (
-    <div
-    style = {{
+    <div className={`${large} menu-item`}>
+        <div className="background-image" 
+        style = {{
         backgroundImage : `url(${image})`
-    }} 
-    className={`${large} menu-item`}>
-        <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className="subtitle">SHOP NOW</span>
-        </div>
+        }} />
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
     </div>
 )
 
